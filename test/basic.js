968727883490296c4e699d06be5b551e87b1996b
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2021-12-12 22:00:25 -0800

    fixed test

--- test/basic.js
+++ test/basic.js
@@ -36,15 +36,23 @@ wire [0:0] FSM_bar = 1;
 wire FSM_foo_bar = ((FSM_state == FSM_foo) & (1));
 wire FSM_bar_foo = ((FSM_state == FSM_bar) & (1));
 
+// FSM state enter conditions
+wire FSM_bar_willEnter = (FSM_foo_bar);
+wire FSM_foo_willEnter = (FSM_bar_foo);
+// FSM state exit conditions
+wire FSM_foo_willExit = (FSM_foo_bar);
+wire FSM_bar_willExit = (FSM_bar_foo);
+
 // FSM next state select
 always @(*) begin : FSM_next_select
   case (1'b1)
     FSM_foo_bar: FSM_next = FSM_bar;
     FSM_bar_foo: FSM_next = FSM_foo;
+    default: FSM_next = FSM_state;
   endcase
 end
 
-always_ff @(posedge clock or negedge reset_n)
+always @(posedge clock or negedge reset_n)
   if (~reset_n) FSM_state <= FSM_foo;
   else          FSM_state <= FSM_next;
 `
